f4caf4a6c9917597b95aa7f8ab1c5e71315e8ffe
Olek Myron <myrondell@gmail.com>
2024-01-19 21:31:05 +0200

    added feature

---
+++ src/js/gallery.js
@@ -0,0 +1,109 @@
+import { fetchImages, pageNumber, PER_PAGE } from './http';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  form: document.querySelector('.js-search-form'),
+  gallery: document.querySelector('.js-list'),
+  loadMoreButton: document.querySelector('.js-load-more'),
+};
+let lightbox, cardHeight;
+
+iziToast.settings({
+  timeout: 5000,
+  resetOnHover: true,
+  icon: 'material-icons',
+  transitionIn: 'flipInX',
+  transitionOut: 'flipOutX',
+  position: 'center',
+});
+
+function renderGallery(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+        largeImageURL,
+      }) =>
+        `<a class="photo-card" aria-role="button" href="${largeImageURL}">
+          <div class="card-thumb">
+            <img src="${webformatURL}" class="card-image" alt="${tags}" loading="lazy" />
+          </div>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${downloads}
+            </p>
+          </div>
+        </a>`
+    )
+    .join('');
+}
+
+async function handleChange(e) {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+  refs.loadMoreButton.classList.add('hidden');
+
+  const query = e.target.elements.searchQuery.value;
+  const { data } = await fetchImages({ query });
+
+  if (!data?.hits.length)
+    return iziToast.error({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again.',
+    });
+
+  iziToast.success({
+    message: `Hooray! We found ${data.totalHits} totalHits images`,
+    messageColor: '#fff',
+    backgroundColor: '#c139f6',
+  });
+
+  refs.gallery.insertAdjacentHTML('beforeend', renderGallery(data.hits));
+  const { height } = refs.gallery.firstElementChild.getBoundingClientRect();
+  cardHeight = height;
+
+  refs.loadMoreButton.classList.remove('hidden');
+  lightbox = new SimpleLightbox('.js-list a');
+}
+
+async function handleLoadMoreButton() {
+  const { data } = await fetchImages({ query: null, isLoadMore: true });
+
+  if (data.totalHits <= pageNumber * PER_PAGE) {
+    refs.loadMoreButton.classList.add('hidden');
+    return iziToast.error({
+      title: 'Error',
+      message: "We're sorry, but you've reached the end of search results.",
+    });
+  }
+
+  lightbox.destroy();
+  refs.gallery.insertAdjacentHTML('beforeend', renderGallery(data.hits));
+  lightbox = new SimpleLightbox('.js-list a');
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+refs.form.addEventListener('submit', handleChange);
+refs.loadMoreButton.addEventListener('click', handleLoadMoreButton);
---
+++ src/js/http.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const PER_PAGE = 40;
+let queryParam = '',
+  pageNumber = 1;
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.params = {
+  key: '22046149-41a2515b5a783e6a5f4bfbfcc',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: PER_PAGE,
+};
+
+async function fetchImages({ query, isLoadMore = false }) {
+  pageNumber = isLoadMore ? pageNumber + 1 : 1
+
+  queryParam = query ?? queryParam;
+
+  return await axios.get('', {
+    params: {
+      q: queryParam,
+      page: pageNumber,
+    },
+  });
+}
+
+export { fetchImages, pageNumber, PER_PAGE };
